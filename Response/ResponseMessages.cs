5f658df3d3f40c729aa93ec4e70ca053d67beb2c
Jagdender <108450877+Jagdender@users.noreply.github.com>
2023-11-26 05:39:51 +0800

    Delete extra Response directory

--- Response/ResponseMessages.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Response
-{
-    public static class ResponseMessages
-    {
-        public const string InvalidParameters =
-            "One or more parameters in your request are invalid.";
-    }
-}
