b6ab5316007b08f344d95242bdd3bbb1d54b4718
Jagdender <b22041814@njupt.edu.cn>
2024-02-09 16:11:08 +0800

    fix(MQ): Fix the bug that MQ couldn't work properly.

--- SNS/SNS.Application/ImageServices/AddImage/AddImageCommandHandler.cs
+++ SNS/SNS.Application/ImageServices/AddImage/AddImageCommandHandler.cs
@@ -1,18 +1,21 @@
-﻿using Primitives.Command;
+﻿using Primitives;
+using Primitives.Command;
 using SNS.Domain.ImageAggregate;
 using SNS.Domain.ImageAggregate.ImageEntity;
 
 namespace SNS.Application.ImageServices.AddImage
 {
-    internal class AddImageCommandHandler(IImageRepository repository)
+    internal class AddImageCommandHandler(IImageRepository repository, IUnitOfWork unitOfWork)
         : ICommandRequestHandler<AddImageCommand>
     {
         private readonly IImageRepository _repository = repository;
+        private readonly IUnitOfWork _unitOfWork = unitOfWork;
 
         public async Task Handle(AddImageCommand request, CancellationToken cancellationToken)
         {
             var image = Image.CreateNewImage(request.ImageId, request.AuthorId, request.AlbumId);
             await _repository.AddNewImageAsync(image, cancellationToken);
+            await _unitOfWork.CommitChangesAsync(cancellationToken);
         }
     }
 }
--- SastImg/SastImg.Infrastructure/Persistence/UnitOfWork.cs
+++ SastImg/SastImg.Infrastructure/Persistence/UnitOfWork.cs
@@ -17,28 +17,23 @@ namespace SastImg.Infrastructure.Persistence
                 cancellationToken
             );
 
-            // First SaveChangesAsync() call is to save the changes made by the command handlers
-            await _dbContext.SaveChangesAsync(cancellationToken);
-
             var domainEntities = _dbContext
                 .ChangeTracker.Entries<IDomainEventContainer>()
                 .Where(x => x.Entity.DomainEvents.Count > 0)
                 .Select(x => x.Entity)
                 .ToList();
 
-            var domainEvents = domainEntities
-                .SelectMany(x =>
-                {
-                    var events = x.DomainEvents;
-                    x.ClearDomainEvents();
-                    return events;
-                })
-                .ToList();
+            // First SaveChangesAsync() call is to save the changes made by the command handlers
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            var domainEvents = domainEntities.SelectMany(x => x.DomainEvents);
 
             var tasks = domainEvents.Select(e => _eventBus.PublishAsync(e));
 
             // Where domain event handlers are executed
             await Task.WhenAll(tasks);
+
+            domainEntities.ForEach(e => e.ClearDomainEvents());
 
             // Second SaveChangesAsync() call is to save the changes made by the domain event handlers
             await _dbContext.SaveChangesAsync(cancellationToken);
